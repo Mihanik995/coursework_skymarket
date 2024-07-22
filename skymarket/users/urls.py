0e6409358f54b4906ad9925710b86545dcde80ca
user <user@example.com>
2024-07-22 19:57:38 +0300

    add token obtain and refresh endpoints

--- skymarket/users/urls.py
+++ skymarket/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from djoser.views import UserViewSet
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 users_router = SimpleRouter()
 
@@ -8,4 +9,7 @@ users_router.register("users", UserViewSet, basename="users")
 
 urlpatterns = [
     path("", include(users_router.urls)),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
