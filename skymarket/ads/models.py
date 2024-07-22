51736a8f91d534a973f49c21e8444ec62ee51604
user <user@example.com>
2024-07-22 16:45:15 +0300

    edit ads and comments models

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,12 +1,36 @@
 from django.conf import settings
 from django.db import models
 
+from users.models import User
+
+NULLABLE = {'null': True, 'blank': True}
+
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    title = models.CharField(max_length=200)
+    price = models.PositiveIntegerField()
+    description = models.CharField(max_length=1000, **NULLABLE)
+    image = models.ImageField(upload_to='ads/', **NULLABLE)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.price})"
+
+    class Meta:
+        verbose_name = 'ad'
+        verbose_name_plural = 'ads'
 
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    text = models.TextField()
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    ad = models.ForeignKey(to=Ad, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'comment'
+        verbose_name_plural = 'comments'
